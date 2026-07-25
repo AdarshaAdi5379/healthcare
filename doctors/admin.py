eb3a1f5ce727f94b56872218cf2e3b8476dca0cc
AdarshaAdi5379 <adarshakk1234@gmail.com>
2026-07-25 21:45:00 +0530

    feat: add Doctor model

--- doctors/admin.py
+++ doctors/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Doctor
 
-# Register your models here.
+
+class DoctorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'specialization', 'email', 'contact_number', 'experience')
+    list_filter = ('specialization',)
+    search_fields = ('name', 'specialization', 'email')
+
+
+admin.site.register(Doctor, DoctorAdmin)
--- doctors/models.py
+++ doctors/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Doctor(models.Model):
+    name = models.CharField(max_length=255)
+    specialization = models.CharField(max_length=255)
+    email = models.EmailField()
+    contact_number = models.CharField(max_length=15)
+    experience = models.IntegerField(default=0)
+    available_from = models.TimeField(null=True, blank=True)
+    available_to = models.TimeField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Dr. {self.name} ({self.specialization})'
