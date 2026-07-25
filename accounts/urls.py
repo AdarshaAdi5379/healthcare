f177b599fe7c6f4816adca6fa789cfa330b618b2
AdarshaAdi5379 <adarshakk1234@gmail.com>
2026-07-25 21:49:36 +0530

    feat: implement registration and login JWT auth endpoints

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,3 +1,7 @@
 from django.urls import path
+from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path('register/', views.register_view, name='auth-register'),
+    path('login/', views.login_view, name='auth-login'),
+]
