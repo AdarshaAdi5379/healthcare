e1534038b15371f365f8f59d676c4adf44a2b15b
AdarshaAdi5379 <adarshakk1234@gmail.com>
2026-07-25 21:46:39 +0530

    feat: add Patient-Doctor Mapping model

--- mappings/admin.py
+++ mappings/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Mapping
 
-# Register your models here.
+
+class MappingAdmin(admin.ModelAdmin):
+    list_display = ('patient', 'doctor', 'assigned_by', 'assigned_at')
+    list_filter = ('assigned_at',)
+    search_fields = ('patient__name', 'doctor__name')
+
+
+admin.site.register(Mapping, MappingAdmin)
--- mappings/models.py
+++ mappings/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Mapping(models.Model):
+    patient = models.ForeignKey(
+        'patients.Patient',
+        on_delete=models.CASCADE,
+        related_name='mappings'
+    )
+    doctor = models.ForeignKey(
+        'doctors.Doctor',
+        on_delete=models.CASCADE,
+        related_name='mappings'
+    )
+    assigned_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='assigned_mappings'
+    )
+    assigned_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('patient', 'doctor')
+        verbose_name_plural = 'mappings'
+
+    def __str__(self):
+        return f'{self.patient.name} -> Dr. {self.doctor.name}'
